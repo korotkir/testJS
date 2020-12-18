e37bbb3e4d16b466b874032a145f9769ae3a887d
Kir <ekoroteev27@yandex.ru>
2020-12-18 17:51:34 +0300

    codewars

--- codewars/app.js
+++ codewars/app.js
@@ -39,6 +39,77 @@ function twoOldestAges(ages) {
 console.log(twoOldestAges([999,1,2,3,7,4,888])) // 1,2,3,6,8,
 
 
+// 5
+
+function descendingOrder(n){
+    let str = String(n)
+    let arr = str.split('')
+    let sort = arr.sort(function(a,b) {
+        return b - a
+    })
+    let result = sort.join('')
+    return +result
+}
+
+console.log(descendingOrder(123456789))
+
+// 6
+
+function sumTwoSmallestNumbers(numbers) {
+    let sorting =  numbers.sort(function(a,b){ return b - a})
+    let result = sorting.slice(-2)
+    return result[0] + result[1]
+}
+
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]))
+
+// 7
+
+function oddOrEven(array) {
+    let sum = 0
+    for(i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    if(sum % 2 === 0) {
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
+
+console.log(oddOrEven([1,2,3,4]))
+
+// 8
+
+function createPhoneNumber(numbers){
+    let str = numbers.join('')
+    return `(${str[0]}${str[1]}${str[2]}) ${str[3]}${str[4]}${str[5]}-${str[6]}${str[7]}${str[8]}${str[9]}`
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // => returns "(123) 456-7890"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
