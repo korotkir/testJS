a0cc6a00a1d600ec231d6b40c82cca83f2b0a8d0
tre3ubec <ekoroteev27@yandex.ru>
2021-02-02 13:23:04 +0300

    added OOP for study

---
+++ OOP/1.Object_Proto_Inherit/app.js
@@ -0,0 +1,6 @@
+user.password = 'hello'
+console.log(user.validatePassword())
+user_profile.username = 'kirill'
+user_profile.age = '22'
+user_profile.photo = false
+console.log(user_profile)
---
+++ OOP/1.Object_Proto_Inherit/user_profile.js
@@ -0,0 +1,9 @@
+let user_profile = {
+    username: '',
+    photo: '',
+    age: 0,
+    __proto__: user
+}
+
+console.log(user_profile)
+
