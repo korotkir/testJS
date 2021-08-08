41b489d717689fdcfda604201cc577aa38987dab
Koroteev Kirill <ekoroteev27@yandex.ru>
2021-08-08 17:36:02 +0300

    Added 15 task

--- codewars/app.js
+++ codewars/app.js
@@ -254,6 +254,31 @@ console.log('test', getMiddle('test')) // es
 console.log('A', getMiddle('A'))
 console.groupEnd()
 
+// 15
+
+function likes(names) {
+  if(names.length === 0) {
+    return `no one likes this`
+  } else if(names.length === 1) {
+    return `${names[0]} likes this`
+  } else if(names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`
+  } else if(names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } else {
+    let value = names.length - 2
+    return `${names[0]}, ${names[1]} and ${value} others like this`
+  }
+}
+
+
+console.groupCollapsed(' 14 ')
+console.log(likes([])) // must be "no one likes this"
+console.log(likes(['Alex',])) // must be "Alex likes this"
+console.log(likes(['Alex', 'Jacob'])) // must be "Alex and Jacob like this"
+console.log(likes(['Alex', 'Jacob', 'Mark'])) // must be "Alex, Jacob and Mark like this"
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max'])) // must be "Alex, Jacob and 2 others like this"
+console.groupEnd()
 
 
 
